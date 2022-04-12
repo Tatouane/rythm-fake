63b7e1962518a0380ad4a06a391d3158d81daa64
Tatouane <86798599+Tatouane@users.noreply.github.com>
2022-04-12 07:50:30 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,46 @@
+const discord = require('discord.js');
+const Client = new discord.Client({intents:[32767]});
+let prefix = 'r!'
+
+Client.on('messageCreate', message =>{
+    //---------------Everyone Role Add Command-------------
+    if(message.content.startsWith(`${prefix}add-role everyone`)){
+        if(message.member.permissions.has('MANAGE_ROLES')){
+            let args = message.content.split(" ");
+            let roleID = args.slice(2).join(" ");
+            let role = message.guild.roles.cache.find(r => r.id === `${roleID}`);
+            
+            if (!role) return message.channel.send(`Veuillez mentionner un rôle.`)
+            message.guild.members.cache.filter(m => !m.user.bot).forEach(member => member.roles.add(role.id)).catch(e =>{
+                console.log(e)
+                message.reply(`Il y a eu une erreur: ${e}`)
+            })
+            message.channel.send(`${message.author} a ajouté le rôle ${role.name} à tout les membres.`)
+        }else{
+            message.reply("Vous n'avez pas les permissions modérateur.");
+        }
+    }
+    //---------------Everyone Role Remove Command-------------
+    if(message.content.startsWith(`${prefix}remove-role everyone`)){
+        if(message.member.permissions.has('MANAGE_ROLES')){
+            let args = message.content.split(" ");
+            let roleID = args.slice(2).join(" ");
+            let role = message.guild.roles.cache.find(r => r.id === `${roleID}`);
+            
+            if (!role) return message.channel.send(`Veuillez mentionner un rôle.`)
+            message.guild.members.cache.filter(m => !m.user.bot).forEach(member => member.roles.remove(role)).catch(e =>{
+                console.log(e)
+                message.reply(`Il y a eu une erreur: ${e}`)
+            })
+            message.channel.send(`${message.author} a enlevé le rôle ${role.name} à tout les membres.`)
+        }else{
+            message.reply("Vous n'avez pas les permissions modérateur.");
+        }
+    }
+})
+
+Client.on('ready', Client =>{
+    console.log('Fake bot ready')
+})
+
+Client.login('OTMzNjYxMjU5NDk1MjUyMDI5.Yekxzg.UKCCZ6wnVD9ZkR8E-YrXg-yXxAE');
